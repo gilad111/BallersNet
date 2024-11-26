1486a88a9dc2ede6f3b64eded47a21398f40d8ca
gilad111 <giladyativ10@gmail.com>
2024-11-26 10:58:46 +0200

    not Working toolBar yet.

--- app/src/main/java/com/example/ballersnet/MainActivity.java
+++ app/src/main/java/com/example/ballersnet/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ballersnet;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +25,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.layout.main_menu, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_player_profile:
+                // קוד לניווט לפרופיל שחקן
+                return true;
+            // טפל בפריטי תפריט נוספים כאן
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
