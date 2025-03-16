90a0b6ecc027950e0077b649e55b2ade460dd42f
gilad111 <giladyativ10@gmail.com>
2025-03-16 18:38:10 +0200

    manage permissions.

--- app/src/main/java/com/example/ballersnet/MainActivity.java
+++ app/src/main/java/com/example/ballersnet/MainActivity.java
@@ -6,12 +6,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.credentials.webauthn.Cbor;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
 // MainActivity serves as the main screen of the application
@@ -29,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
        // toolbar.setLogo(R.drawable.app_logo);
         }
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu
@@ -38,6 +41,32 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
 
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        // Check if user is admin
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        database.getReference("Users").child(userId).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                User user = task.getResult().getValue(User.class);
+                if (user != null) {
+                    if (user.isAdmin) {
+                        // User is admin, show "Create Team" button
+                        MenuItem createTeamButton = menu.findItem(R.id.action_create_team);
+                        createTeamButton.setVisible(true);
+                    } else {
+                        // User is not admin, hide "Create Team" button
+                        MenuItem createTeamButton = menu.findItem(R.id.action_create_team);
+                        createTeamButton.setVisible(false);
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return true;
     }
 
     @SuppressLint("NonConstantResourceId")
