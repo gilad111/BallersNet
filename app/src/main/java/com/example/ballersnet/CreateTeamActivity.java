130b318e4bfe1713a45e81c4a1ac53e0ba728596
gilad111 <giladyativ10@gmail.com>
2025-03-16 18:34:30 +0200

    Save a team as a team

--- app/src/main/java/com/example/ballersnet/CreateTeamActivity.java
+++ app/src/main/java/com/example/ballersnet/CreateTeamActivity.java
@@ -56,62 +56,67 @@ public class CreateTeamActivity extends MainActivity {
         // Get current user
         String userId = mAuth.getCurrentUser().getUid();
 
-        // Check if user is a team manager
-        mDatabase.child("Users").child(userId).get().addOnCompleteListener(task -> {
-            if (task.isSuccessful()) {
-                User user = task.getResult().getValue(User.class);
-                if (user != null) {
-                    if (user.isAdmin) {
-                        // User is a team manager, proceed with team creation
-                        if (user.teamName == null || user.teamName.isEmpty()) {
-                            // User doesn't have a team, create new team
-                            String teamName = teamNameEditText.getText().toString().trim();
-                            String homeCourt = homeCourtEditText.getText().toString().trim();
-                            String neededPositions = neededPositionsEditText.getText().toString().trim();
+        if (teamNameEditText.getText().toString().isEmpty() ||
+                homeCourtEditText.getText().toString().isEmpty() ||
+                neededPositionsEditText.getText().toString().isEmpty())
+        {
+            Toast.makeText(CreateTeamActivity.this, "Fill all the fields", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            // Check if user is a team manager
+            mDatabase.child("Users").child(userId).get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    User user = task.getResult().getValue(User.class);
+                    if (user != null) {
+                        if (user.isAdmin) {
+                            // User is a team manager, proceed with team creation
+                            if (user.teamName == null || user.teamName.isEmpty()) {
+                                // User doesn't have a team, create new team
+                                String teamName = teamNameEditText.getText().toString().trim();
+                                String homeCourt = homeCourtEditText.getText().toString().trim();
+                                String neededPositions = neededPositionsEditText.getText().toString().trim();
 
-                            // Check if team name is already taken
-                            mDatabase.child("Teams").orderByChild("name").equalTo(teamName).addListenerForSingleValueEvent(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                    if (dataSnapshot.exists()) {
-                                        Toast.makeText(CreateTeamActivity.this, "Team name is already taken", Toast.LENGTH_SHORT).show();
-                                    } else {
-                                        // Create new Team object
-                                        Team newTeam = new Team(teamName, homeCourt, neededPositions, user.name);
+                                // Check if team name is already taken
+                                mDatabase.child("Teams").orderByChild("name").equalTo(teamName).addListenerForSingleValueEvent(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                        if (dataSnapshot.exists()) {
+                                            Toast.makeText(CreateTeamActivity.this, "Team name is already taken", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            // Create new Team object
+                                            Team newTeam = new Team(teamName, homeCourt, neededPositions, user.name);
 
-                                        // Generate a new unique key for the team
-                                        String teamId = mDatabase.child("Teams").push().getKey();
-
-                                        // Save the team to Firebase
-                                        mDatabase.child("Teams").child(teamId).setValue(newTeam)
-                                                .addOnSuccessListener(aVoid -> {
-                                                    // Update user's teamId
-                                                    mDatabase.child("Users").child(userId).child("teamId").setValue(teamId);
-                                                    Toast.makeText(CreateTeamActivity.this, "Team created successfully", Toast.LENGTH_SHORT).show();
-                                                    finish(); // Close the activity
-                                                })
-                                                .addOnFailureListener(e -> Toast.makeText(CreateTeamActivity.this, "Failed to create team", Toast.LENGTH_SHORT).show());
+                                            // Save the team to Firebase
+                                            mDatabase.child("Teams").child(teamName).setValue(newTeam)
+                                                    .addOnSuccessListener(aVoid -> {
+                                                        // Update user's teamName
+                                                        mDatabase.child("Users").child(userId).child("teamName").setValue(teamName);
+                                                        Toast.makeText(CreateTeamActivity.this, "Team created successfully", Toast.LENGTH_SHORT).show();
+                                                        finish(); // Close the activity
+                                                    })
+                                                    .addOnFailureListener(e -> Toast.makeText(CreateTeamActivity.this, "Failed to create team", Toast.LENGTH_SHORT).show());
+                                        }
                                     }
-                                }
 
-                                @Override
-                                public void onCancelled(@NonNull DatabaseError databaseError) {
-                                    Toast.makeText(CreateTeamActivity.this, "Failed to check team name", Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                                        Toast.makeText(CreateTeamActivity.this, "Failed to check team name", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            } else {
+                                // User already has a team
+                                Toast.makeText(CreateTeamActivity.this, "You already have a team", Toast.LENGTH_SHORT).show();
+                            }
                         } else {
-                            // User already has a team
-                            Toast.makeText(CreateTeamActivity.this, "You already have a team", Toast.LENGTH_SHORT).show();
+                            // User is not a team manager
+                            Toast.makeText(CreateTeamActivity.this, "Only team managers can create teams", Toast.LENGTH_SHORT).show();
                         }
-                    } else {
-                        // User is not a team manager
-                        Toast.makeText(CreateTeamActivity.this, "Only team managers can create teams", Toast.LENGTH_SHORT).show();
                     }
+                } else {
+                    Toast.makeText(CreateTeamActivity.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
                 }
-            } else {
-                Toast.makeText(CreateTeamActivity.this, "Failed to retrieve user data", Toast.LENGTH_SHORT).show();
-            }
-        });
+            });
+        }
     }
 
 }
